2d3f0bcb1686c8a074ae2e8942bac8262a560b99
carojane <carojane@gmail.com>
2014-09-23 13:48:15 -0400

    user views list of buses spec test

---
+++ spec/features/user_views_list_of_buses_spec.rb
@@ -0,0 +1,29 @@
+feature "User views list of buses", %Q{
+  As a prospective user
+  I want to see a list of buses
+  So that I can select one to read and post reviews
+
+  Acceptance Criteria:
+
+  The bus number must be listed in order
+  There is a link to each individual bus page
+  The list must include the end destinations
+  } do
+
+  scenario "listed in order" do
+    visit buses_path
+
+    expect(page).to have_content "1 - Harvard/Holyoke Gate - Dudley Station via Massachusetts Avenue"
+  end
+
+  scenario "links to individual bus page" do
+    click_on "1 - Harvard/Holyoke Gate - Dudley Station via Massachusetts Avenue"
+
+    expect
+  end
+
+  scenario "The list must include the end destinations" do
+    expect(page).to have_content "Harvard/Holyoke Gate"
+  end
+end
+
