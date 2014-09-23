639beae016555a23fc4509fa6c26b25ea9b5c5c5
carojane <carojane@gmail.com>
2014-09-23 19:52:49 -0400

    fix line length, whitespace

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,8 +1,16 @@
 class Ride < ActiveRecord::Base
   belongs_to :bus
 
-  validates :timeframe, presence: true, uniqueness: { scope: [ :direction, :bus, :day       ] }
-  validates :direction, presence: true, uniqueness: { scope: [ :timeframe, :bus, :day       ] }
-  validates :day,       presence: true, uniqueness: { scope: [ :timeframe, :bus, :direction ] }
-  validates :bus,       presence: true, uniqueness: { scope: [ :timeframe, :day, :direction ] }
+  validates :timeframe,
+            presence: true,
+            uniqueness: { scope: [ :direction, :bus, :day ] }
+  validates :direction,
+            presence: true,
+            uniqueness: { scope: [ :timeframe, :bus, :day ] }
+  validates :day,
+            presence: true,
+            uniqueness: { scope: [ :timeframe, :bus, :direction ] }
+  validates :bus,
+            presence: true,
+            uniqueness: { scope: [ :timeframe, :day, :direction ] }
 end
