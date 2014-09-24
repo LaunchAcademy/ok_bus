c9f31f1a1381b55104c493cacdd653088cec084b
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-09-24 10:15:41 -0400

    Reviews nested route established.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'buses#index'
+
+  resources :rides do
+    resources :reviews, only: [:new, :create, :show]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
