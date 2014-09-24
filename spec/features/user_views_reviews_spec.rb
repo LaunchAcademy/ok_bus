7ed998b712acbaabdaef7218f39c1a5c5f49c0e3
carojane <carojane@gmail.com>
2014-09-24 16:27:09 -0400

    fixed factory_girl test models

--- spec/features/user_views_reviews_spec.rb
+++ spec/features/user_views_reviews_spec.rb
@@ -5,10 +5,9 @@ feature "User views all reviews for bus", %{
   So that I can read other people's opinions
   } do
   scenario "must be on bus detail page" do
-    bus = FactoryGirl.create(:bus)
-    visit bus_path(bus.id)
-    review = FactoryGirl.build(:review)
-    save_and_open_page
+    review = FactoryGirl.create(:review)
+    bus = review.ride.bus
+    visit bus_path(bus)
     expect(page).to have_content review.rating.to_s + " out of 5"
     expect(page).to have_content review.body
   end
@@ -16,10 +15,10 @@ feature "User views all reviews for bus", %{
   scenario "only see reviews associated with bus"
 
   scenario "reviews are in order with most recent first" do
-    bus = FactoryGirl.create(:bus)
-    visit bus_path(bus.id)
-    review1 = FactoryGirl.build(:review)
-    review2 = FactoryGirl.build(:review)
+    review1 = FactoryGirl.create(:review)
+    review2 = FactoryGirl.create(:review, ride: review1.ride)
+    bus = review1.ride.bus
+    visit bus_path(bus)
 
     if review1.created_at > review2.created_at
       expect(review1.body).to appear_before review2.body
@@ -29,10 +28,10 @@ feature "User views all reviews for bus", %{
   end
 
   scenario "review shows ride details" do
-    bus = FactoryGirl.create(:bus)
-    visit bus_path(bus.id)
-    ride = FactoryGirl.create(:ride)
-    review = FactoryGirl.build(:review)
+    review = FactoryGirl.create(:review)
+    bus = review.ride.bus
+    ride = review.ride
+    visit bus_path(bus)
     expect(page).to have_content ride.timeframe
     expect(page).to have_content ride.direction
     expect(page).to have_content ride.day
