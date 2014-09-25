72977c4054b3469be6a0de5365b74cdc250f711a
Jonathan Loh <jonrloh@gmail.com>
2014-09-25 13:51:21 -0400

    admins can delete users

--- spec/features/admin_functions_spec.rb
+++ spec/features/admin_functions_spec.rb
@@ -12,26 +12,30 @@ feature "Admin fuctionality", %{
 
   context "authenticated admin" do
     before :each do
-      @admin = FactoryGirl.create(:user)
-      @admin.update_attribute :admin, true
+      @admin = FactoryGirl.create(:user, admin: true)
+      # @admin.update_attribute :admin, true
       @user = FactoryGirl.create(:user)
     end
 
     scenario "admin visits user index" do
       sign_in_as(@admin)
-      binding.pry
       visit "/admin/users"
+      save_and_open_page
       expect(page).to have_content(@user.email)
     end
 
     scenario "Unauthorized users cannot access user index" do
       sign_in_as(@user)
       visit "/admin/users"
-      save_and_open_page
       expect(page).to have_content("not authorized")
     end
 
     scenario "admin deletes another user's account" do
+      sign_in_as(@admin)
+      visit "/admin/users"
+      # binding.pry
+      # save_and_open_page
+      click_on "Delete"
     end
   end
 end
