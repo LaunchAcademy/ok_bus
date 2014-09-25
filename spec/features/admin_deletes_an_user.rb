0cf2d5ccf1eeb8aea9ebd7c3e27974c4381b5d07
Otavio <otamm@hotmail.com>
2014-09-25 12:07:13 -0400

    Adapted specs

--- spec/features/admin_deletes_an_user.rb
+++ spec/features/admin_deletes_an_user.rb
@@ -1,17 +1,18 @@
 require "rails_helper"
 
-
-let(:user) { FactoryGirl.create(:user)}
-let(:review) { Review.create(body: "aaa",user_id: user.id,rating: rand(1..5)) }
 context "authenticated admin" do
   before :each do
+    @user = FactoryGirl.create(:user)
     @admin = FactoryGirl.create(:user)
+    @review = Review.create(body: "aaa",user_id: @user.id, rating: rand(1..5))
     @admin.update_attribute :admin,true
     sign_in_as(@admin)
   end
+
   scenario "admin is logged in as an admin" do
     @admin.admin?
   end
+
   scenario "admin deletes someone else's review" do
 
 
