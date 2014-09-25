88640fe268f24f4688f284349db5652da621b905
Otavio <otamm@hotmail.com>
2014-09-25 11:34:02 -0400

    Corrected the review creation on spec

--- spec/features/admin_deletes_an_user.rb
+++ spec/features/admin_deletes_an_user.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 
 let(:user) { FactoryGirl.create(:user)}
-let(:review) { user.review }
+let(:review) { Review.create(body: "aaa",user_id: user.id,rating: rand(1..5)) }
 context "authenticated admin" do
   before :each do
     @admin = FactoryGirl.create(:user)
