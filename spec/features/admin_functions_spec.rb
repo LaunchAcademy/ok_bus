5f3bb88b394d7047ba110f82e06c03b352b303c5
Jonathan Loh <jonrloh@gmail.com>
2014-09-25 12:13:39 -0400

    fixed spec file to user let method

--- spec/features/admin_functions_spec.rb
+++ spec/features/admin_functions_spec.rb
@@ -1,17 +1,29 @@
 require "rails_helper"
 
+feature "Admin fuctionality", %{
+  As an admin, I should be identified as such
+  I should have more functions than a regular user
+  So that I can maintain order on the site.
 
-let(:user) { FactoryGirl.create(:user)}
+  Acceptance Criteria:
+
+  I must be logged in and identified as an admin
+} do
+
+let(:user) { FactoryGirl.create(:user) }
 let(:review) { Review.create(body: "aaa",user_id: user.id,rating: rand(1..5)) }
+
 context "authenticated admin" do
   before :each do
     @admin = FactoryGirl.create(:user)
     @admin.update_attribute :admin,true
     sign_in_as(@admin)
   end
+
   scenario "admin is logged in as an admin" do
     @admin.admin?
   end
+
   scenario "admin deletes someone else's review" do
 
 
@@ -23,4 +35,5 @@ context "authenticated admin" do
 
   end
 
+end
 end
