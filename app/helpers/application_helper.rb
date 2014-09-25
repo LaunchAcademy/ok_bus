062974743b8b9739f2255f787d921a6cdae29f23
Jonathan Loh <jonrloh@gmail.com>
2014-09-25 12:54:11 -0400

    admin can now visit user index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def is_admin?
+    self.admin?
+  end
+
 end
--- spec/features/admin_functions_spec.rb
+++ spec/features/admin_functions_spec.rb
@@ -8,32 +8,29 @@ feature "Admin fuctionality", %{
   Acceptance Criteria:
 
   I must be logged in and identified as an admin
-} do
+  } do
 
-let(:user) { FactoryGirl.create(:user) }
-let(:review) { Review.create(body: "aaa",user_id: user.id,rating: rand(1..5)) }
+  context "authenticated admin" do
+    before :each do
+      @admin = FactoryGirl.create(:user)
+      @admin.update_attribute :admin, true
+      @user = FactoryGirl.create(:user)
+    end
 
-context "authenticated admin" do
-  before :each do
-    @admin = FactoryGirl.create(:user)
-    @admin.update_attribute :admin,true
-    sign_in_as(@admin)
+    scenario "admin visits user index" do
+      sign_in_as(@admin)
+      visit "/admin/users"
+      expect(page).to have_content(@user.email)
+    end
+
+    scenario "Unauthorized users cannot access user index" do
+      sign_in_as(@user)
+      visit "/admin/users"
+      save_and_open_page
+      expect(page).to have_content("not authorized")
+    end
+
+    scenario "admin deletes another user's account" do
+    end
   end
-
-  scenario "admin is logged in as an admin" do
-    @admin.admin?
-  end
-
-  scenario "admin deletes someone else's review" do
-
-
-
-  end
-
-  scenario "admin deletes another user's account" do
-
-
-  end
-
-end
 end
