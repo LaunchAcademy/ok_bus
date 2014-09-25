95c4749d9243ccfb2566de4788747ba3cf67ecd6
Jonathan Loh <jonrloh@gmail.com>
2014-09-25 16:19:49 -0400

    limited admin functionality over users to only index and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :users
+    resources :users, only: [:index, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
