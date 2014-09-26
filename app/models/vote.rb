e7cf580bd2a67b7570623a7c5598658b6c0549ee
carojane <carojane@gmail.com>
2014-09-26 11:31:42 -0400

    forgot comma

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :review
 
-  validates :direction, presence: true
+  validates :direction, presence: true,
                         inclusion: { "up", "down" }
   validates :user, presence: true
   validates :review, presence: true
