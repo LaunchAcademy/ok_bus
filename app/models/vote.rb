82f2903257ad03acc07b8397d9332df9ee4621c8
carojane <carojane@gmail.com>
2014-09-26 11:47:39 -0400

    uniqueness scope user and review for vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,8 @@ class Vote < ActiveRecord::Base
   belongs_to :review
 
   validates :direction, presence: true,
-                        inclusion: { ["up", "down"] }
+                        inclusion: { ["up", "down"] },
   validates :user, presence: true
+                   uniqueness: { scope: [:review] }
   validates :review, presence: true
 end
