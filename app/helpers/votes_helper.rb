1715418ce22a37881224bd5561d912e3fc60bdeb
carojane <carojane@gmail.com>
2014-09-26 15:37:35 -0400

    helper method for if statement

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,20 @@
 module VotesHelper
+  def change_or_vote(bus, review, direction)
+    vote = Vote.find_by(review_id: review.id, user_id: current_user.id)
+    if vote
+      if vote.direction != direction.downcase
+        link_to direction,
+        bus_review_vote_path(bus,
+                             review,
+                             vote), method: :PUT
+      else
+        direction
+      end
+    else
+      link_to direction,
+      bus_review_votes_path(bus,
+                            review,
+                            direction: direction), method: :POST
+    end
+  end
 end
