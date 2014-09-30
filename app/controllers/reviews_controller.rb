fa62b98cc35d9c0ffcd1f4a99ff2ec9308a2f785
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-09-30 10:03:35 -0400

    Format edits.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,7 +14,7 @@ class ReviewsController < ApplicationController
 
     if @review.save
       redirect_to bus_path(@bus),
-      notice: "Review successfully created."
+        notice: "Review successfully created."
     else
       render "new"
     end
@@ -30,7 +30,7 @@ class ReviewsController < ApplicationController
 
     if @review.update(review_params)
       redirect_to bus_path(@review.ride.bus),
-      notice: "Review successfully updated."
+        notice: "Review successfully updated."
     else
       render "edit"
     end
@@ -41,7 +41,7 @@ class ReviewsController < ApplicationController
     @bus = @review.ride.bus
     @review.destroy
     redirect_to bus_path(@bus),
-    notice: "Review successfully deleted."
+      notice: "Review successfully deleted."
   end
 
 
