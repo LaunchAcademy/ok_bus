f6f42c49624b3eae0cefd50173860602c3ac3954
Jonathan Loh <jonrloh@gmail.com>
2014-09-30 14:33:49 -0400

    tests now pass

--- spec/features/user_can_view_profile_page.rb
+++ spec/features/user_can_view_profile_page.rb
@@ -1,17 +1,53 @@
 require "rails_helper"
 
 feature "User can view profile page", %{
+  I want to be able to view my profile
   I want to be able to update my information
   I want to see the reviews I have posted
   } do
 
-  scenario "update user info" do
-    
+  let(:ride) { FactoryGirl.create(:ride) }
+  let(:bus) { ride.bus }
+
+  context "authenticated user" do
+    before :each do
+      @user = FactoryGirl.create(:user)
+      sign_in_as(@user)
+    end
+
+    scenario "view profile page" do
+      click_on "User Profile"
+      expect(page).to have_content(@user.username)
+      expect(page).to have_content(@user.email)
+      expect(page).to have_content "Your reviews"
+    end
+
+    scenario "update user info" do
+      click_on "User Profile"
+      click_on "Edit Info"
+
+      fill_in "Password", with: @user.password + "1"
+      fill_in "Password confirmation", with: @user.password + "1"
+      fill_in "Current password", with: @user.password
+      attach_file "Profile photo", "app/assets/images/sushi.jpg"
+
+      click_on "Update"
+      expect(page).to have_content "You updated your account successfully"
+    end
+
+    scenario "view all posted reviews" do
+      review = FactoryGirl.build(:review, user: @user, ride: ride)
+
+      visit bus_path(bus)
+      click_on "New Review"
+
+      select review.ride.description, from: "review[ride_id]"
+      select review.rating, from: "review[rating]"
+      fill_in "Body", with: review.body
+
+      click_on "Create Review"
+      click_on "User Profile"
+      expect(page).to have_content(review.body)
+    end
   end
-
-  scenario "view all posted reviews" do
-
-  end
-
-
 end
