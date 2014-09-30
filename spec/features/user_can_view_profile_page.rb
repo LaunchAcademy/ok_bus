6dfaf81a6fe5f379b45a06a5da99fb89f854bab9
Jonathan Loh <jonrloh@gmail.com>
2014-09-30 10:57:55 -0400

    created spec to test for user profile page

---
+++ spec/features/user_can_view_profile_page.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "User can view profile page", %{
+  I want to be able to update my information
+  I want to see the reviews I have posted
+  } do
+
+  scenario "update user info" do
+    
+  end
+
+  scenario "view all posted reviews" do
+
+  end
+
+
+end
