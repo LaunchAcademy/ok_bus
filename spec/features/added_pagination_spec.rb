291dec361c9664e8f2189611333f0503ad8fd4a8
Otavio <otamm@hotmail.com>
2014-09-30 11:39:40 -0400

    Polished pagination

--- spec/features/added_pagination_spec.rb
+++ spec/features/added_pagination_spec.rb
@@ -1,33 +1,25 @@
-require "rails_helper"
+require 'rails_helper'
 
 feature "Pagination" do
 
   context "browsing website" do
-    before :each do
+    before :all do
       @admin = FactoryGirl.create(:user, admin: true)
       @user = FactoryGirl.create(:user)
-      @dummy_user1 = FactoryGirl.create(:user)
-      @dummy_user2 = FactoryGirl.create(:user)
-      @dummy_user3 = FactoryGirl.create(:user)
-      @dummy_bus1 = FactoryGirl.create(:bus)
-      @dummy_bus2 = FactoryGirl.create(:bus)
-      @dummy_bus3 = FactoryGirl.create(:bus)
-      @dummy_bus4 = FactoryGirl.create(:bus)
     end
 
     scenario "admin visits user index" do
       sign_in_as(@admin)
-      visit "/admin/users"
+      visit admin_users_path
       click_button 'Next'
       expect(response).to redirect_to('/admin/users?page=2')
     end
 
     scenario "user sees buses" do
       sign_in_as(@user)
-      visit "/buses"
+      visit buses_path
       click_button 'Next'
       expect(response).to redirect_to('/buses?page=2')
     end
-
   end
 end
