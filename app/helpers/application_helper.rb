0f2e3170fbe13f3927545509b1ba7fb2e7d717da
Jonathan Loh <jonrloh@gmail.com>
2014-10-01 10:18:16 -0400

    removed gravatar code that was accidentally added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  #
-  #    def gravatar_url(user)
-  #   gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-  #   "http://gravatar.com/avatar/#{gravatar_id}.png"
-  # end
-
 end
