327aa23c49e8a4d19e963d4eae57330032de9741
carojane <carojane@gmail.com>
2014-10-01 17:11:03 -0400

    change all to each

--- spec/features/added_pagination_spec.rb
+++ spec/features/added_pagination_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature "Pagination" do
 
   context "browsing website" do
-    before :all do
+    before :each do
       @admin = FactoryGirl.create(:user, admin: true)
       @user = FactoryGirl.create(:user)
       @users = FactoryGirl.create_list(:user,User.default_per_page)
