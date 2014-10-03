d2d56b062f6db64e20edcba1c187d925b4aea801
carojane <carojane@gmail.com>
2014-10-03 11:56:03 -0400

    hound

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -6,14 +6,12 @@ if Rails.env.development?
   }
 end
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    port:       587,
-    address:    "smtp.mandrillapp.com",
-    user_name:  ENV["MANDRILL_USERNAME"],
-    password:   ENV["MANDRILL_APIKEY"],
-    domain:     "heroku.com",
-    authentication: :plain
-  }
-
-
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  port:       587,
+  address:    "smtp.mandrillapp.com",
+  user_name:  ENV["MANDRILL_USERNAME"],
+  password:   ENV["MANDRILL_APIKEY"],
+  domain:     "heroku.com",
+  authentication: :plain
+}
