96b52391ce58b39e6479d11a17b356030461e1c3
Helen Hood <helen.c.hood@gmail.com>
2014-10-03 10:18:19 -0400

    Refactor pagination spec

--- spec/features/added_pagination_spec.rb
+++ spec/features/added_pagination_spec.rb
@@ -3,19 +3,24 @@ require 'rails_helper'
 feature "Pagination" do
 
   context "browsing website" do
-      @admin = User.find_by(admin: true)
-#      26.times {FactoryGirl.create(:user)}
-#      26.times {FactoryGirl.create(:bus)}
-
     scenario "admin visits user index" do
-      sign_in_as(@admin)
+      26.times { FactoryGirl.create(:user) }
+      admin = FactoryGirl.create(:user, admin: true)
+
+      sign_in_as(admin)
       visit admin_users_path
-      page.should have_selector('nav.pagination')
+
+      expect(page).to have_selector('nav.pagination')
+      expect(page).to have_link "Next"
     end
 
     scenario "user sees buses" do
+      26.times {FactoryGirl.create(:bus)}
+
       visit root_path
-      page.should have_selector('nav.pagination')
+
+      expect(page).to have_selector('nav.pagination')
+      expect(page).to have_link "Next"
     end
   end
 end
